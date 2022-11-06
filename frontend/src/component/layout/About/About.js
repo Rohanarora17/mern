07c77d6111554567ebac3b65a9d7e36d1fdf63fe
unknown <rohanarora494@gmail.com>
2022-11-06 05:32:25 +0530

    website

---
+++ frontend/src/component/layout/About/About.js
@@ -0,0 +1,38 @@
+import React from "react";
+import "./aboutSection.css";
+import { Button, Typography, Avatar } from "@material-ui/core";
+import YouTubeIcon from "@material-ui/icons/YouTube";
+import InstagramIcon from "@material-ui/icons/Instagram";
+const About = () => {
+  
+  return (
+    <div className="aboutSection">
+      <div></div>
+      <div className="aboutSectionGradient"></div>
+      <div className="aboutSectionContainer">
+        <Typography component="h1">About Us</Typography>
+
+        <div>
+          <div>
+            <Avatar
+              style={{ width: "10vmax", height: "10vmax", margin: "2vmax 0" }}
+              src=""
+              alt="Founder"
+            />
+            <Typography>Varun Sharma</Typography>
+            
+            
+            <span>
+            <br></br>
+              Hi everyone!
+              I am Varun and this is my project for summer training . 
+            </span>
+          </div>
+        
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
